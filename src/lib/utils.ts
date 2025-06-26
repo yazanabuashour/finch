157fb1c1be83813c079a15aa4f3afbe272451b6a
Yazan Abuashour <y.ashour123@gmail.com>
2025-06-26 17:34:15 -0500

    refactor(utils.ts): use `toISOString` to format date
    The `toISOString` method provides a more concise and standard way to
    format dates as `YYYY-MM-DD`, reducing code complexity.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatDate(date: Date): string {
-  const year: number = date.getFullYear();
-  const month: string = String(date.getMonth() + 1).padStart(2, "0");
-  const day: string = String(date.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
+  return date.toISOString().slice(0, 10);
 }
